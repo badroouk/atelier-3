80c86dab8d8fe3521f396a4f606f928200058332
badroouk <75887046+badroouk@users.noreply.github.com>
2021-12-09 23:31:45 +0100

    Update exercice1.cpp

--- exercice1.cpp
+++ exercice1.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
-
 using namespace std;
+class myclass{ // dceclaration de la classe
+private:
+    string nom;
+public:
+    myclass();//declaration de constructeur
+    ~myclass();//declaration de destructeur
+};
+myclass::myclass(){
+    cout<< "entrez votre nom:"<<endl;//demender a l'utilisateur de d'entrer le nom
+    cin >> nom;
+    cout<< "hello : "<< nom <<endl; //message affiche
+}
+myclass::~myclass(){
+    cout<< "good bye : "<< nom <<endl;
+}
+int main(){
 
-int main()
-{
-    cout << "Hello world!" << endl;
+
+    myclass c1; //l'objet de classe 'myclass'
+
+
     return 0;
 }
